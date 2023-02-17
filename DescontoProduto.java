f61293c2d6a2f56e63d7e0f1055b0d7f5cc4e5eb
lmbernardo7520112 <lmbernardo752011@gmail.com>
2023-02-17 18:47:10 -0300

    Desconto Produto

---
+++ DescontoProduto.java
@@ -0,0 +1,13 @@
+import javax.swing.JOptionPane;
+
+public class DescontoProduto {
+    public static void main(String[] args) {
+        double valorProduto = Double.parseDouble(JOptionPane.showInputDialog("Digite o valor do produto:"));
+        double percentualDesconto = Double.parseDouble(JOptionPane.showInputDialog("Digite o percentual de desconto:"));
+        
+        double valorDesconto = valorProduto * percentualDesconto / 100;
+        double valorProdutoComDesconto = valorProduto - valorDesconto;
+        
+        JOptionPane.showMessageDialog(null, "Valor do desconto: R$ " + valorDesconto + "\nValor do produto com desconto: R$ " + valorProdutoComDesconto);
+    }
+}
