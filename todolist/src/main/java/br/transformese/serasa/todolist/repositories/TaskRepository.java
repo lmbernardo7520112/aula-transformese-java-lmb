81e45a55ada02335552ebc9b8310fe1d620ef8e8
root <root@DESKTOP-IUFN7JG>
2023-05-19 08:23:28 -0300

    Refatorando TaskRepository e Taskservice.

--- todolist/src/main/java/br/transformese/serasa/todolist/repositories/TaskRepository.java
+++ todolist/src/main/java/br/transformese/serasa/todolist/repositories/TaskRepository.java
@@ -5,12 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-  
+
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
-    public Task findByTask(String task);
-    public List<Task> findByCompletedTrue();
-    public List<Task> findByCompletedFalse();
-    public List<Task> findAll();
-    public Task getById(Long id);
+    Task findByTask(String task);
+    List<Task> findByCompletedTrue();
+    List<Task> findByCompletedFalse();
 }
--- todolist/src/main/java/br/transformese/serasa/todolist/services/TaskService.java
+++ todolist/src/main/java/br/transformese/serasa/todolist/services/TaskService.java
@@ -4,38 +4,42 @@ import br.transformese.serasa.todolist.models.Task;
 import br.transformese.serasa.todolist.repositories.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-  
+
 import java.util.List;
-  
+
 @Service
 public class TaskService {
+    private final TaskRepository taskRepository;
+
     @Autowired
-    private TaskRepository taskRepository;
-      
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
     public Task createNewTask(Task task) {
         return taskRepository.save(task);
     }
-      
+
     public List<Task> getAllTask() {
         return taskRepository.findAll();
     }
-      
+
     public Task findTaskById(Long id) {
-        return taskRepository.getById(id);
+        return taskRepository.findById(id).orElse(null);
     }
-      
+
     public List<Task> findAllCompletedTask() {
         return taskRepository.findByCompletedTrue();
     }
-      
-    public List<Task> findAllInCompleteTask() {
+
+    public List<Task> findAllIncompleteTask() {
         return taskRepository.findByCompletedFalse();
     }
-      
+
     public void deleteTask(Long id) {
         taskRepository.deleteById(id);
     }
-      
+
     public Task updateTask(Task task) {
         return taskRepository.save(task);
     }
