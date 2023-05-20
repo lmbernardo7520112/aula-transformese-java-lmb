85894da16ad8c6eed6354c74736d9135303f28cc
root <root@DESKTOP-IUFN7JG>
2023-05-20 17:31:50 -0300

    Renderizando o formulário.

--- todolist/src/main/java/br/transformese/serasa/todolist/controllers/TaskController.java
+++ todolist/src/main/java/br/transformese/serasa/todolist/controllers/TaskController.java
@@ -5,12 +5,14 @@ import br.transformese.serasa.todolist.services.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Controller
 @RequestMapping("/api/v1/tasks")
+//@RequestMapping("/api/v1/tasks")
 public class TaskController {
 
     @Autowired
@@ -18,7 +20,7 @@ public class TaskController {
 
     
 
-    @GetMapping("")
+    /*@GetMapping("")
     public String getAllTasks(Model model) {
         List<Task> tasks = taskService.getAllTask();
         Task newTask = new Task();
@@ -26,17 +28,33 @@ public class TaskController {
         model.addAttribute("newTask", newTask);
         //model.addAttribute("newTask", new Task());
         return "index";
-    }
+    }*/
+
+    @GetMapping("")
+    public String getAllTasks(Model model) {
+    List<Task> tasks = taskService.getAllTask();
+    Task newTask = new Task();
+    model.addAttribute("tasks", tasks);
+    model.addAttribute("newTask", newTask);
+    return "index";
+}
 
     @GetMapping("/")
     public String index(@ModelAttribute("newTask") Task newTask) {
         return "index";
     }
 
-    @PostMapping("")
+    /*@PostMapping("")
     public String createTask(Task task, Model model) {
         taskService.createNewTask(task);
         model.addAttribute("newTask", task);
+        return "redirect:/api/v1/tasks";
+    }*/
+
+    @PostMapping("")
+    public String createTask(@ModelAttribute("newTask") Task task, Model model) {
+        taskService.createNewTask(task);
+        model.addAttribute("newTask", new Task());
         return "redirect:/api/v1/tasks";
     }
 
